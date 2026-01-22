dcfa704ce1528ee68a405e31afcfcce7ed79ec55
TeevincsCrypt <teevincs1@gmail.com>
2026-01-22 04:52:43 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,20 @@
 const duels = [];
+
+// This mimics what smart contracts will return later
+const duels = [
+  {
+    id: "duel-001",
+    teamA: {
+      name: "Team Alpha",
+      symbol: "ALPHA",
+      marketCap: 42000
+    },
+    teamB: {
+      name: "Team Beta",
+      symbol: "BETA",
+      marketCap: 38000
+    },
+    cap: 69000,
+    status: "live" // live | finished
+  }
+];
